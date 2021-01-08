00a36b8ca3235f07274d402868ff116885567939
aag112 <76956429+aag112@users.noreply.github.com>
2021-01-08 11:54:27 -0500

    .

---
+++ pythonfiles/familyslicing.py
@@ -0,0 +1,11 @@
+family = ['Sandra','Juan','Jhoan','Devin','Adrian','Daniela','Isa','Hernan','Mylenne','Sebas','Alejandro','Haydee','Percy','Gerardo','Dani','Adriana','Ruth','Viviana','Wagner','Damian']
+print(family[0:3])
+print(family[2:4])
+print(family[:3])
+print(family[2:])
+print(family[-2:])
+print(family[1:5])
+print(family[:-1])
+print(family[-1:])
+print(family[-3:])
+print(family[8:9])
